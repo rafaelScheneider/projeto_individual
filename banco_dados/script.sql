43ca478fdced6b8b94a2e5060a4ed621b15ba488
rafaelSchimas <rafael.ranaudo@sptech.school>
2023-05-25 23:25:31 -0300

    Preparando a forja

--- banco_dados/script.sql
+++ banco_dados/script.sql
@@ -18,9 +18,7 @@ CREATE TABLE carta (
   FkUser INT,
   nome VARCHAR(40) NOT NULL,
   custo VARCHAR(10) NOT NULL,
-  tipo VARCHAR(30) NOT NULL,
-  superTipo VARCHAR(20),
-  subTipo VARCHAR(20) NOT NULL,
+  tipo VARCHAR(60) NOT NULL,
   cor VARCHAR(20),
   efeito VARCHAR(100),
   FOREIGN KEY (FkUser) REFERENCES user(idUser)
